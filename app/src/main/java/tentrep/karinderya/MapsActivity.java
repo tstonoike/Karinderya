abd47d7a396e27ba399db0c5791c2c1f8eef976d
tstonoike <tstonoike@yahoo.com>
2016-08-27 08:17:51 +0800

    added new activity functionality

--- app/src/main/java/tentrep/karinderya/MapsActivity.java
+++ app/src/main/java/tentrep/karinderya/MapsActivity.java
@@ -1,14 +1,20 @@
 package tentrep.karinderya;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -21,10 +27,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback ,GoogleMap.OnMarkerClickListener {
     private MarkerOptions options = new MarkerOptions();
     private ArrayList<LatLng> latlngs = new ArrayList<>();
+    HashMap<String, String> markerMap = new HashMap<String, String>();
+    String globe = null;
     JSONObject json = null;
     String str = "";
     HttpResponse response;
@@ -33,7 +42,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     String[] array1 = new String[]{};
     String[] array3 = new String[]{};
     String[] array5 = new String[]{};
+    Bundle bundle = new Bundle();
+    //String[] array7 = new String[]{};
+    public int num=0;
+    public int index3=0;
     private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,25 +60,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
     }
-
     private class GetTextViewData extends AsyncTask<Void, Void, Void> {
         public Context context;
 
         public GetTextViewData(Context context) {
             this.context = context;
         }
-
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
         }
-
         @Override
         protected Void doInBackground(Void... arg0) {
 
             HttpClient myClient = new DefaultHttpClient();
-           HttpPost myConnection = new HttpPost("http://masarapp.azurewebsites.net/index.php");
-          //  HttpPost myConnection = new HttpPost("http://127.0.0.1/masarapp/send-data.php");
+           // HttpPost myConnection = new HttpPost("http://masarapp.azurewebsites.net/index.php");
+           HttpPost myConnection = new HttpPost("http://192.168.1.12:80/masarapp/send-data.php");
             try {
                 response = myClient.execute(myConnection);
                 str = EntityUtils.toString(response.getEntity(), "UTF-8");
@@ -75,7 +86,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 e.printStackTrace();
             }
 
-
             try {
 
                 jArray = new JSONArray(str);
@@ -83,16 +93,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 String[] array2 = new String[total];
                 String[] array4 = new String[total];
                 String[] array6 = new String[total];
+                String[] array8 = new String[total];
                 for(int i=0;i<total;i++) {
                     json = jArray.getJSONObject(i);
                     array2[i] = json.getString("karinderya_lat");
                     array4[i] = json.getString("karinderya_long");
                     array6[i] = json.getString("karinderya_name");
 
+               //     array8[i] = json.getString("karindeya_id");
+
                 }
                 array1=array2.clone();
                 array3=array4.clone();
                 array5=array6.clone();
+            //    array7=array8.clone();
+
             } catch (JSONException e) {
                 e.printStackTrace();
             } catch (Exception e) {
@@ -106,10 +121,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        googleMap.setOnMarkerClickListener(this);
+
 
         LatLng APC = new LatLng(14.531037, 121.021453);
         mMap.addMarker(new MarkerOptions().position(APC).title("Marker in APC"));
-
         for(int index=0; index<array1.length; index++)
        {
         latlngs.add(new LatLng(Float.valueOf(array1[index]), Float.valueOf(array3[index])));
@@ -118,11 +134,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         {
             options.position(latlngs.get(index));
             options.title(array5[index]);
-            googleMap.addMarker(options);
 
+            googleMap.addMarker(options);
 
         }
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(APC));
+
+        mMap.setOnMarkerClickListener(this);
+    }
+    @Override
+    public boolean onMarkerClick(Marker loc) {
+        Intent intent = new Intent(this, stars.class);
+
+       for (index3=0; index3 < array1.length; index3++) {
+            if (loc.getTitle().equals(array5[index3])){
+                bundle.putString("name", loc.getTitle());
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        }
+
+        return true;
     }
 }
+
+
